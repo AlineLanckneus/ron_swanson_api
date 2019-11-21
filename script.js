9c39365a129c4b1bf0d698c35a17e212f0de82d2
Aline L <aline.lanckneus@live.be>
2019-11-21 12:05:01 +0100

    created array with 5 quotes

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-const quoteValue = document.body.querySelector(".quote");
+const quoteValue1 = document.querySelector(".quote1");
+const quoteValue2 = document.querySelector(".quote2");
+const quoteValue3 = document.querySelector(".quote3");
+let newArray = [];
+
 
 async function quoteGenerator(){
-    const response = await fetch("http://ron-swanson-quotes.herokuapp.com/v2/quotes");
+    const response = await fetch("https://ron-swanson-quotes.herokuapp.com/v2/quotes/200");
     const data = await response.json();
+    //console.log(typeof data);
+    //console.log(data);
+
     
-    console.log(data);
-    quoteValue.innerHTML = data;
-}
+        data.forEach(function(quote) {
+            if (quote === "Clear alcohols are for rich women on diets."){
+                //console.log(quote);
+                newArray.unshift(quote);
+            } else if(quote === "Fishing relaxes me. It's like yoga, except I still get to kill something.") {
+                //console.log(quote);
+                newArray.unshift(quote);
+            } else if(quote === "Turkey can never beat cow."){
+                //console.log(quote);
+                newArray.unshift(quote);
+            } else if(quote === "Capitalism: God's way of determining who is smart and who is poor."){
+                //console.log(quote);
+                newArray.unshift(quote);
+            } else if(quote === "Creativity is for people with glasses who like to lie."){
+                //console.log(quote);
+                newArray.unshift(quote);
+            }
+            
+        });
+        newArray.sort();                                 
+        for (let i = 0; i < newArray.length; i++) {
+            console.log(newArray[i]);
+            
+        }
+        
+};
 
 quoteGenerator();
