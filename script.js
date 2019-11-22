b881ebd1392a38e4704c344a1b8aeb4619a0fb3b
Aline L <aline.lanckneus@live.be>
2019-11-22 16:50:07 +0100

    almost done add random quote bottom page

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const quoteValue3 = document.querySelector(".quote3");
 const quoteValue4 = document.querySelector(".quote4");
 const quoteValue5 = document.querySelector(".quote5");
 let newArray = [];
+const r = document.querySelector("#random");
 
 let src1 = "/images/ron-swanson-bottom-arrow.png";
 let src2 = "/images/ron-swanson-middle-arrow.png";
@@ -17,34 +18,32 @@ function refreshPage(){
 async function quoteGenerator(){
     const response = await fetch("https://ron-swanson-quotes.herokuapp.com/v2/quotes/200");
     const data = await response.json();
-    //console.log(typeof data);
-    //console.log(data);
+
+    
 
         //iterates over all of the quotes looking for 5 specific quotes and adding these to an empty array
         data.forEach(function(quote) {
             if (quote === "Clear alcohols are for rich women on diets."){
-                //console.log(quote);
+                
                 newArray.unshift(quote);
             } else if(quote === "Fishing relaxes me. It's like yoga, except I still get to kill something.") {
-                //console.log(quote);
+                
                 newArray.unshift(quote);
             } else if(quote === "Turkey can never beat cow."){
-                //console.log(quote);
+                
                 newArray.unshift(quote);
             } else if(quote === "Capitalism: God's way of determining who is smart and who is poor."){
-                //console.log(quote);
+                
                 newArray.unshift(quote);
             } else if(quote === "Creativity is for people with glasses who like to lie."){
-                //console.log(quote);
+
                 newArray.unshift(quote);
             }
         });//end of foreach
         //sorts the array of quotes in a fixed and alphabetical way
         newArray.sort();                                 
         for (let i = 0; i < newArray.length; i++) {
-            //console.log(newArray[i]);
-            //newArray[i]=string
-            //console.log(newArray[0]);
+
         }//end of for loop
         //assigning quotes to variables
         let firstQuote = newArray[0];
@@ -52,24 +51,12 @@ async function quoteGenerator(){
         let thirdQuote = newArray[2];
         let fourthQuote = newArray[3];
         let fifthQuote = newArray[4];
-/*
-        console.log(firstQuote);
-        console.log(secondQuote);
-        console.log(thirdQuote);
-        console.log(fourthQuote);
-        console.log(fifthQuote);
-*/
         //replacing keyword by blank space
         let one = firstQuote.replace('Capitalism','__________ ');
         let two = secondQuote.replace('alcohols', '________');
         let three = thirdQuote.replace('Creativity', '__________');
         let four = fourthQuote.replace('Fishing', '_______');
         let five = fifthQuote.replace('Turkey', '______');
-        console.log(one);
-        console.log(two);
-        console.log(three);
-        console.log(four);
-        console.log(five);
         //assigning each corresponding html element with a quote
         quoteValue1.innerHTML = one;
         quoteValue2.innerHTML = two;
@@ -91,7 +78,7 @@ let inputValue5 = document.getElementById("input5");
 //checks if answers are correct and adds to score
 //different image will pop up with each score
 const highScore = "Well done! You correctly guessed most of the answers! You've earned your place at the top of the Pyramid of Greatness!";
-const avgScore = "Not bad! You correctly guessed three out of five. Your place in the Pyramid of Greatness is subsequently somewhere in the middle. Hit the try again button to do better!";
+const avgScore = "Not bad! You guessed three out of five. Your place in the Pyramid of Greatness is subsequently somewhere in the middle. Hit the try again button to do better!";
 const lowScore = "Auwtsch, you've barely earned your place in the Pyramid of Greatness.. Keep dangling at the bottom or be a true Swanson and hit the try again button to do better!";
 
 
@@ -133,7 +120,7 @@ function imagePopUp(){
     if (inputValue5.value === 'Turkey' && inputValue5.value !== null){
         counter++};
     console.log(counter);
-    
+
     //displaying different image for each score
     if(counter >= 4){
         console.log("winner!")
@@ -162,6 +149,32 @@ function imagePopUp(){
 document.getElementById("submit").addEventListener("click", function(){
     imagePopUp();
 });
+/*
+let randomQuoteEl = document.querySelector(".randomQuote");
 
+async function randomQuoteGenerator(){
+    const response = await fetch("https://ron-swanson-quotes.herokuapp.com/v2/quotes/200");
+    const data = await response.json();
+    return data[0];
+    function r(){
+        randomQuoteEl.innerHTML = data[0];
+    };
+};
 
+document.querySelector("#random").addEventListener("click", function(){
+    randomQuoteGenerator();
+});*/
 
+async function rquoteGenerator(){
+    const response = await fetch("https://ron-swanson-quotes.herokuapp.com/v2/quotes/200");
+    const data = await response.json();
+
+    
+        for (let i = 0; i < data.length; i++) {
+        r.innerHTML = data[i];};
+    };
+    
+
+    r.addEventListener("click", function(){
+        rquoteGenerator();
+    });
